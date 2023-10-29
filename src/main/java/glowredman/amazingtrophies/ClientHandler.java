9071e1b4fbd0c23fcd2f4bceee62fac4a6110071
glowredman <35727266+glowredman@users.noreply.github.com>
2023-10-29 21:31:30 +0100

    Add entity trophy model handler

--- src/main/java/glowredman/amazingtrophies/ClientHandler.java
+++ src/main/java/glowredman/amazingtrophies/ClientHandler.java
@@ -14,6 +14,7 @@ import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.common.Loader;
 import glowredman.amazingtrophies.api.AmazingTrophiesAPI;
 import glowredman.amazingtrophies.model.BasicTrophyModelHandler;
+import glowredman.amazingtrophies.model.EntityTrophyModelHandler;
 import glowredman.amazingtrophies.model.PedestalTrophyModelHandler;
 import glowredman.amazingtrophies.trophy.RendererTrophy;
 import glowredman.amazingtrophies.trophy.TileEntityTrophy;
@@ -35,6 +36,7 @@ public class ClientHandler {
         // spotless:off
         AmazingTrophiesAPI.registerTrophyModelHandlerProvider(PedestalTrophyModelHandler.ID, PedestalTrophyModelHandler::new);
         AmazingTrophiesAPI.registerTrophyModelHandlerProvider(BasicTrophyModelHandler.ID, BasicTrophyModelHandler::new);
+        AmazingTrophiesAPI.registerTrophyModelHandlerProvider(EntityTrophyModelHandler.ID, EntityTrophyModelHandler::new);
         // spotless:on
     }
 
