44202aa1ac4e8d2948a922df254af994655d34e5
glowredman <35727266+glowredman@users.noreply.github.com>
2023-11-11 18:38:33 +0100

    Remove redundant classes

--- src/main/java/glowredman/amazingtrophies/api/StructureRenderer/Trophies/BaseTrophyItemRenderer.java
+++
@@ -1,9 +0,0 @@
-package glowredman.amazingtrophies.api.StructureRenderer.Trophies;
-
-
-import glowredman.amazingtrophies.api.StructureRenderer.Base.BaseRenderItemRenderer;
-
-public class BaseTrophyItemRenderer extends BaseRenderItemRenderer {
-
-
-}
