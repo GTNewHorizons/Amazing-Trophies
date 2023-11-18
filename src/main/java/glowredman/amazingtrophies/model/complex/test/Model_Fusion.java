ae316760727c40c730a62de8e2d253ba36943dbb
Connor Colenso <colen@CONNORSPC>
2023-11-18 02:10:53 +0000

    Remove

--- src/main/java/glowredman/amazingtrophies/model/complex/test/Model_Fusion.java
+++
@@ -1,8 +0,0 @@
-package glowredman.amazingtrophies.model.complex.test;
-
-import glowredman.amazingtrophies.model.complex.BaseModelStructure;
-
-public class Model_Fusion extends BaseModelStructure {
-
-
-}
