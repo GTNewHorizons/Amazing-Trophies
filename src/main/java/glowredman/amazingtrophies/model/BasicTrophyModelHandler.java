d0b45e164d42acf04b476e333fd2b16ecb87cb4c
Jason Mitchell <mitchej@gmail.com>
2023-12-31 16:14:00 -0800

    Include basic trophys

--- src/main/java/glowredman/amazingtrophies/model/BasicTrophyModelHandler.java
+++ src/main/java/glowredman/amazingtrophies/model/BasicTrophyModelHandler.java
@@ -1,19 +1,16 @@
 package glowredman.amazingtrophies.model;
 
-import javax.annotation.Nullable;
-
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+import com.gtnewhorizons.angelica.mixins.interfaces.IModelCustomExt;
+import glowredman.amazingtrophies.AssetHandler;
+import glowredman.amazingtrophies.ConfigHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.AdvancedModelLoader;
-import net.minecraftforge.client.model.IModelCustom;
-
 import org.lwjgl.opengl.GL11;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSyntaxException;
-
-import glowredman.amazingtrophies.AssetHandler;
-import glowredman.amazingtrophies.ConfigHandler;
+import javax.annotation.Nullable;
 
 public class BasicTrophyModelHandler extends PedestalTrophyModelHandler {
 
@@ -21,19 +18,19 @@ public class BasicTrophyModelHandler extends PedestalTrophyModelHandler {
     public static final String PROPERTY_MODEL = "model";
     public static final String PROPERTY_TEXTURE = "texture";
 
-    private IModelCustom model;
+    private IModelCustomExt model;
     private ResourceLocation texture;
 
     public BasicTrophyModelHandler() {}
 
-    public BasicTrophyModelHandler(IModelCustom model, ResourceLocation texture) {
+    public BasicTrophyModelHandler(IModelCustomExt model, ResourceLocation texture) {
         this.model = model;
         this.texture = texture;
     }
 
     @Override
     public void parse(String id, JsonObject json) throws JsonSyntaxException {
-        this.model = AdvancedModelLoader.loadModel(
+        this.model = (IModelCustomExt) AdvancedModelLoader.loadModel(
             AssetHandler.getResourceLocation(ConfigHandler.getStringProperty(json, PROPERTY_MODEL), "models/"));
         this.texture = AssetHandler
             .getResourceLocation(ConfigHandler.getStringProperty(json, PROPERTY_TEXTURE), "textures/blocks/");
@@ -51,7 +48,7 @@ public class BasicTrophyModelHandler extends PedestalTrophyModelHandler {
         GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
         GL11.glTranslated(x, y, z);
         GL11.glRotatef(22.5f * rotation, 0.0f, 1.0f, 0.0f);
-        this.model.renderAll();
+        this.model.renderAllVBO();
         GL11.glPopMatrix();
     }
 
